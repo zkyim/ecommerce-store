f4bc990a59b476ccd5908a5267da13a07254beca
zkym <zkym4564@gmail.com>
2024-08-12 20:35:53 +0300

    change billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,13 +8,13 @@ export const revalidate = 0;
 
 const HomePage = async () => {
     const products = await getProducts({ isFeaturedId: true });
-    const billboard = await getBillboards('e274d0ae-ba97-4258-98ed-044d26c898d9');
+    const billboard = await getBillboards('9c5cdfc6-d32c-473e-9bc5-1fb86e7ec1b1');
     
     return (
         <div>
             <Container>
                 <div className="space-y-10 pd-10">
-                    {/* <Billboard data={billboard}/> */}
+                    <Billboard data={billboard}/>
                 </div>
                 <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
                     <ProductList title="Featured Products" items={products} />
